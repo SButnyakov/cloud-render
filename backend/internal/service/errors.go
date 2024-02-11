5477b5b0bc962be6fc44164b70c4da84f12ad2da
Sergey <sergey.butnyakov@gmail.com>
2024-02-11 23:16:45 +0300

    create info endpoint

--- backend/internal/service/errors.go
+++ backend/internal/service/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUserAlreadyExists  = errors.New("user already exsits")
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUserNotFound       = errors.New("user not found")
 )
